788ebfc977d9365144b3c68f2c685b0aaa64c890
Swati Khedekar <87851640+Swatikhedekar@users.noreply.github.com>
2026-04-21 14:40:09 +0530

    Add files via upload

---
+++ sql_scripts/2_stage_creation - Copy.sql
@@ -0,0 +1,45 @@
+--CREATE FILE FORMAT AND STAGE
+CREATE OR REPLACE FILE FORMAT csv_format 
+TYPE = 'CSV' 
+FIELD_DELIMITER = ',' 
+SKIP_HEADER = 1 
+ERROR_ON_COLUMN_COUNT_MISMATCH = FALSE;
+
+SHOW FILE FORMATS;
+
+CREATE OR REPLACE STAGE snowstage 
+FILE_FORMAT = csv_format 
+URL = 's3://snowbucket-sk/source/';
+
+
+SHOW STAGES;
+-- insert data from aws s3 to snowflake
+COPY INTO AIRBNB_DB.STAGING_SCHEMA.BOOKINGS
+FROM
+    @snowstage FILES =('bookings.csv') CREDENTIALS =(
+        aws_key_id = '***',
+        aws_secret_key = '******'
+        );
+
+COPY INTO AIRBNB_DB.STAGING_SCHEMA.HOSTS
+FROM
+    @snowstage FILES =('hosts.csv') CREDENTIALS =(
+        aws_key_id = '***',
+        aws_secret_key = '****'
+        );
+        
+COPY INTO AIRBNB_DB.STAGING_SCHEMA.LISTINGS   
+FROM
+    @snowstage FILES =('listings.csv') CREDENTIALS =(
+        aws_key_id = '******',
+        aws_secret_key = '*****'
+        );
+        
+-- SEE THE DATA IN TABLE
+SELECT COUNT(*) FROM BOOKINGS;
+SELECT COUNT(*) FROM HOSTS;
+SELECT COUNT(*) FROM LISTINGS;
+
+SELECT * FROM BOOKINGS;
+SELECT * FROM HOSTS;
+SELECT * FROM LISTINGS;
